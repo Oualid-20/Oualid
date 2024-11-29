ffaaa9c7915b49c97160564880b465f34a40bd08
Oualid-20 <oualidbellaoui9@gmail.com>
2024-11-29 01:17:04 +0100

    Add partials and some variable for TemplateController

--- src/Controller/TemplateController.php
+++ src/Controller/TemplateController.php
@@ -11,8 +11,12 @@ class TemplateController extends AbstractController
     #[Route('/template', name: 'app_template')]
     public function index(): Response
     {
+        $text = "I am Template";
+        $tabYear = [2020,2021,2022,2023,2024];
         return $this->render('template/index.html.twig', [
-            'controller_name' => 'TemplateController',
+            // 'controller_name' => 'TemplateController',
+            'text' => $text,
+            'years' => $tabYear
         ]);
     }
 }
